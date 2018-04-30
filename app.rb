801d270c979224f734605ba1c456b01667ffab06
zee <zperezcanals@gmail.com>
2018-04-30 13:17:56 -0400

    MVP minus edit complete

--- app.rb
+++ app.rb
@@ -22,14 +22,9 @@ end
 
 #Logged in state homepage  ITOWRRRRKKKKSSSS DONT TOUCH
 
-get '/home' do 	
+get '/home' do 
 	@user = User.find(session[:id])
-	if @user != nil
-		session[:id]= @user.id
 	erb :home,:layout => :layout_loggedin
-	else
-		redirect :'/signup'
-	end
 end
 
 # signup route for signup form  ITOWRRRRKKKKSSSS DONT TOUCH
@@ -37,23 +32,24 @@ get '/signup' do
 	erb :signup
 end
 
+
 #signup ITOWRRRRKKKKSSSS DONT TOUCH
 post '/signup' do
 	@user = User.create(first_name: params[:firstname], last_name: params[:lastname], email: params[:email], username: params[:username], password: params[:password], birthday: params[:birthday])
 	session[:id]= @user.id
-	erb :profile, :layout => :layout_loggedin
+	erb :home, :layout => :layout_loggedin
 end
 
 #Logged in profile   ITOWRRRRKKKKSSSS DONT TOUCH
-get '/profile' do 
+get '/admin' do 
 	@user = User.find(session[:id])
-	if @user != nil
-		session[:id]= @user.id
-	erb :profile, :layout => :layout_loggedin
-	else 
-		redirect :'/signup'
-	end
+	erb :admin, :layout => :layout_loggedin
 end
+
+# get '/admin/:id' do 
+# 	@user = User.find(session[:id])
+# 	erb :admin, :layout => :layout_loggedin
+# end
 
 # Login get and Login post BOTH WORKKKKKKKK DONT TOUCH!!!
 get '/login' do
@@ -64,24 +60,84 @@ post '/login' do
 	@user = User.find_by(username: params[:username],password: params[:password])
 	if @user != nil
 		session[:id]= @user.id
-		erb :home, :layout => :layout_loggedin
+		@post = Post.where(user_id: session[:id])
+		redirect '/home'
+		# erb :home, :layout => :layout_loggedin
 	else 
 		redirect :'/signup'
 	end
 end
 
-#New post route
+# Logout
+get '/logout' do
+	session.clear
+	redirect :'/login'
+end
+
+#New get and post route
 get '/new' do 
-	@user = User.find(session[:id])
-	if @user != nil
-		session[:id]= @user.id
 	erb :new, :layout => :layout_loggedin
-	else
-		redirect :'/signup'
-	end
 end
 
  post '/new' do 
  	@post = Post.create(title:params[:title],body:params[:body],date: params[:date],user_id: session[:id])
- 	redirect :'/profile'
+ 	redirect :'/admin'	
  end
+
+
+ get '/myblog' do
+ 	@post = Post.where(user_id: session[:id])
+ 	erb :myblog, :layout => :layout_loggedin
+ end
+
+ get '/home' do
+ 	@user = User.find(session[:id])
+ 	@post = Post.where(user_id: session[:id])
+ 	erb :home, :layout => :layout_loggedin 
+ end
+
+
+# Show all users in "Social" page
+
+get '/social' do
+	@users = User.all
+	erb :social, :layout => :layout_loggedin 
+end
+
+
+# Setup other user's profile and blog one page
+get '/profile' do
+	@specific_profile = User.find(params[:id])
+	erb :profile, :layout => :layout_loggedin
+end
+
+get '/profile/:id' do
+	@specific_profile = User.find(params[:id])
+	@post = Post.where(user_id: @specific_profile.id)
+	erb :profile, :layout => :layout_loggedin
+end
+
+
+# Render form for editing a new post (U)
+get 'myblog/:id/edit' do 
+    @specific_post = Post.find(params[:id])
+    erb :edit, :layout => :layout_loggedin
+end
+
+
+#Edit post from /id/edit route (U)
+# put 'myblog/:id' do
+#     @specific_post = Post.find(params[:id])
+#     @specific_post.update(title:params[:title],body:params[:body],date: params[:date],user_id: session[:id])
+#     redirect :'/myblog'
+# end
+  
+# Delete Attendee (D)
+delete '/admin' do
+	@current_user = session[:id]
+    User.destroy(@current_user)
+    session.clear
+    redirect :'/signup'
+end
+
+
